603dc8c2101c6f173041e502d3195135809f035c
SowmiyaDuraibabuRepo <sowmiya.duraibabu@bnpparibasfortis.com>
2025-04-03 23:45:57 +0530

    Create SulfurasUpdater.java

---
+++ src/main/java/com/gilledrose/v2/SulfurasUpdater.java
@@ -0,0 +1,10 @@
+package com.gildedrose.v2;
+
+import com.gildedrose.v1.Item;
+
+public class SulfurasUpdater implements QualityUpdater{
+    @Override
+    public void updateQuality(Item item) {
+
+    }
+}
